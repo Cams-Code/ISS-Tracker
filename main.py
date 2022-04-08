735f394c93e8976bfd443a68114fd4dcd00f5c48
Cameron <camparker290599@gmail.com>
2022-04-08 10:44:52 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime
+import smtplib
+import os
+
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASS = os.environ.get("MY_PASS")
+
+# my longitude/latitude
+MY_LAT = os.environ.get("MY_LAT")
+MY_LONG = os.environ.get("MY_LONG")
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+iss_close = False
+
+if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+    iss_close = True
+if iss_close:
+    if time_now.hour < sunrise or time_now.hour > sunset:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASS)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="camparker290599@gmail.com",
+                msg="Subject: ISS Ping\n\n Look up! There is the ISS!\n"
+            )
